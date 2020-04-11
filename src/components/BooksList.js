bf9281960d0b7917b0288e9e858739345773fa9f
Karan Aditya <karanaditya993@gmail.com>
2020-04-10 20:24:55 -0400

    feat: Add search page functionality with easter egg :)

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { PropTypes  }  from 'prop-types'
 import formatShelf from '../helpers/formatShelf.js'
+import BooksResults from './BooksResults.js'
 
 export default class BooksList extends Component {
     static propTypes = {
@@ -14,7 +15,7 @@ export default class BooksList extends Component {
         return(
             <div className="list-books">
                 <div className="list-books-title">
-                    <h1>My Reads : {new Date().getFullYear()}</h1>
+                    <h1>My {new Date().getFullYear()} Reading Library</h1>
                 </div>
                 <div className="list-books-content">
                     {Object.keys(books).map(shelf =>
@@ -22,29 +23,7 @@ export default class BooksList extends Component {
                             <h2 className="bookshelf-title">{formatShelf(shelf)}</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                    {books[shelf].map((book) =>
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className='book-cover'
-                                                         style={{backgroundImage: `url(${book.imageLinks.thumbnail})`}}
-                                                    >
-                                                    </div>
-                                                    <div className="book-shelf-changer">
-                                                          <select>
-                                                            <option value="move" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                          </select>
-                                                    </div>
-                                                </div>
-                                                <div className="book-title">{book.title}</div>
-                                                <div className="book-authors">{book.authors[0]}</div>
-                                            </div>
-                                        </li>
-                                    )}
+                                    <BooksResults books={books[shelf]} />
                                 </ol>
                             </div>
                         </div>
